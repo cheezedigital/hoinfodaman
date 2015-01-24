a7ddcdca1df0f3bc2712a0ee19616e95a9222090
Justin Veirs <justinveirs@gmail.com>
2015-01-24 18:18:40 -0500

    remove end

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ActiveRecord::Base
   belongs_to :user
+
   has_many :sales
 
-  validates_numericality_of :price
-    greater_than: 40
+  validates_numericality_of :price,
+    greater_than: 49,
     message: "must be at least 50 cents"
 
   has_attached_file :file
