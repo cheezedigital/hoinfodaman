feae221d1cd43612a83738f4b5c48d29e23403a3
Justin Veirs <justinveirs@gmail.com>
2015-01-25 17:01:22 -0500

    madrill username established by heroku config:get MANDRILL_USERNAME

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,4 +35,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {
     :host => 'https://ancient-retreat-5593.herokuapp.com/'
   }
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
