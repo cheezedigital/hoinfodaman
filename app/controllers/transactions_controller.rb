292fad7f5ff3001e408c8641bf86a6020cd6ae4c
Justin Veirs <justinveirs@gmail.com>
2015-01-26 09:42:22 -0500

    all types of issues

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,11 +1,10 @@
 class TransactionsController < ApplicationController
-  skip_before_action :authenticate_user!,
-    only: [:new, :create]
+  skip_before_action :authenticate_user!, only: [:new, :create]
 
   def new
-    @product = Product.find_by!(
-      permalink: params[:permalink]
-    )
+    @product = Product.new #find_by!(
+      #permalink: params[:permalink]
+    #)
   end
 
   def pickup
